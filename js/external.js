f19d04242a2af5d1f1a2fa0fea43b659385467ab
couxal22 <couxal22@gmail.com>
2019-08-06 07:54:51 +0545

    css

---
+++ js/external.js
@@ -0,0 +1,17 @@
+/*
+var a=10;
+b=20;
+let c=a+b;
+
+document.write("the sum of "+a+" and "+b+" is " +c)
+*/
+function greeting()
+{
+    alert("WELCOME :) ")
+}
+function userName()
+{
+    var name;
+    name=prompt("enter your name?");
+document.getElementsByID('name').innerHTML="WELCOME,"+name;
+}
